84ae5cb603cef5542f8f91c4d0000eacca6a114c
Alex <galanthusan@gmail.com>
2018-02-08 17:46:59 +0200

    Velocity.js, FilterComponent gather form data

--- src/components/ProductSidebarComponent.vue
+++ src/components/ProductSidebarComponent.vue
@@ -6,10 +6,10 @@
                 <h2>Тип нагрева</h2>
                 <div class="input-list">
                     <div class="form-group">
-                        <label><input type="checkbox" name="heater_type" value="external" id="heater_type_external">Открытый ТЭН</label>
+                        <label><input type="checkbox" name="heater_type_external" value="true" id="heater_type_external" v-model="filterData.heater_type_external">Открытый ТЭН</label>
                     </div>
                     <div class="form-group">
-                        <label><input type="checkbox" name="heater_type" value="internal" id="heater_type_internal">Закрытый ТЭН</label>
+                        <label><input type="checkbox" name="heater_type_internal" value="true" id="heater_type_internal" v-model="filterData.heater_type_internal">Закрытый ТЭН</label>
                     </div>
                 </div>
             </div>
@@ -18,29 +18,8 @@
         <div class="filter-group">
             <h2>Производитель</h2>
             <div class="input-list">
-                <div class="form-group">
-                    <label> <input type="checkbox" name="brand" value="ariston" id="brand-ariston">Ariston</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="brand" value="atlantic" id="brand-atlantic">Atlantic</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="brand" value="bosch" id="brand-bosch">Bosch</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="brand" value="electrolux" id="brand-electrolux">Electrolux</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="brand" value="gorenje" id="brand-gorenje">Gorenje</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox"name="brand" value="roda" id="brand-roda">Roda</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="brand" value="willer" id="brand-willer">Willer</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="brand" value="zanussi" id="brand-zanussi">Zanussi</label>
+                <div class="form-group" v-for="brand in filterData.brands">
+                    <label> <input type="checkbox" v-bind:name="brand.name" :value="brand.value" id="brand-ariston" v-model="brand.checked">{{brand.name}}</label>
                 </div>
             </div>
         </div>
@@ -48,26 +27,8 @@
         <div class="filter-group">
             <h2>Объем, л</h2>
             <div class="input-list">
-                <div class="form-group">
-                    <label><input type="checkbox" name="amount" value="30l" id="amount-30">менее 30</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="amount" value="30-49l" id="amount-30-49">30 - 49</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="amount" value="50-79l" id="amount-50-79">50 - 79</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="amount" value="80-99l" id="amount-80-99">80 - 99</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="amount" value="100-149l" id="amount-100-149">100 - 149</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="amount" value="150-199l" id="amount-150-199">150 - 199</label>
-                </div>
-                <div class="form-group">
-                    <label><input type="checkbox" name="amount" value="200l" id="amount-200">свыше 200</label>
+                <div class="form-group" v-for="amount in filterData.amounts">
+                    <label><input type="checkbox" name="amount" v-bind:value="amount.value" id="amount-30" v-model="amount.checked">{{amount.name}}</label>
                 </div>
             </div>
         </div>
@@ -79,11 +40,42 @@
 
 <script>
   export default {
-    name: 'HelloWorld',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        filterData: {
+           heater_type_external: '',
+           heater_type_internal: '',
+           brands:  [
+             { name : 'Ariston', value: 'ariston', checked: false },
+             { name : 'Atlantic', value: 'atlantic', checked: false },
+             { name : 'Bosch', value: 'bosch', checked: false },
+             { name : 'Electrolux', value: 'electrolux', checked: false },
+             { name : 'Gorenje', value: 'gorenje', checked: false },
+             { name : 'Roda', value: 'roda', checked: false },
+             { name : 'Willer', value: 'willer', checked: false },
+             { name : 'Zanussi', value: 'zanussi', checked: false },
+           ],
+           amounts: [
+             { name : 'менее 30', value: '30l', checked: false },
+             { name : '30 - 49', value: '30-49l', checked: false },
+             { name : '50 - 79', value: '50-79l', checked: false },
+             { name : '80 - 99', value: '80-99l', checked: false },
+             { name : '100 - 149', value: '100-149l', checked: false },
+             { name : '150 - 199', value: '150-199l', checked: false },
+             { name : 'свыше 200', value: '200l', checked: false },
+           ]
+        },
       }
+    },
+    watch: {
+      filterData: {
+        handler: function (value) { // watch it
+          console.log(value);
+          // this.$parent.onFilterUpdate(value);
+        },
+        deep: 1
+      },
     }
   }
 </script>
+
